24e2ac812de15d7860a142b8e6ba08ce40ed5f28
brendiih <brendiih13@gmail.com>
2026-08-25 11:39:51 +0700

    Add files via upload

---
+++ b4.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	/*selection part 2*/
+	int b = 3;
+	switch (b){
+		case 1:
+		printf ("JOKOWI");
+		break;
+		case 2:
+		printf ("PRABOWO");
+		break;
+		default:
+		printf ("SELAIN JOKOWI DAN PRABOWO");
+	}
+	
+	return 0;	
+}
