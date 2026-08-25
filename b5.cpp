f92b3be0a8612da0b48e1647e1cfaefeec810238
brendiih <brendiih13@gmail.com>
2026-08-25 11:40:07 +0700

    Add files via upload

---
+++ b5.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	/*selection part3*/
+	int angka = 688;
+	printf("%s\n", (angka%2 == 0? "genap" : "ganjil"));
+	
+	printf("%s", (angka < 2? "lebih kecil dari 2" : "lebih besar dari 2"));
+	
+	return 0;
+
+
+
+
+
+
+
+
+
+}
