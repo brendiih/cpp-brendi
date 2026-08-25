a3e0834a046aff4e41534f523989ad2b3bc89af8
brendiih <brendiih13@gmail.com>
2026-08-25 11:39:35 +0700

    Add files via upload

---
+++ b3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+	/*selection part 1*/
+	
+	int angka = 8 ;
+	
+	if (angka==0){
+		printf("nol");
+	}
+	else if (angka%2==1){
+		printf("ganjil");
+	}
+	else if (angka%2==0){
+		printf("genap");
+	}
+	
+	return 0;
+	
+	}
